6491421eec5362dd559bd3951da8e5944e7f93e9
Alexandr Lazarev <diedsmiling@gmail.com>
2019-07-25 08:24:30 +0200

    Make starRegistry a promise

--- packages/registry/src/index.ts
+++ packages/registry/src/index.ts
@@ -1,5 +1,8 @@
 import startRegistry from './server'
 
 startRegistry({
-  
+  port: 3000
+}).then(server => {
+  console.log(server.listening)
+  console.log(`Loom Registry server started on: ${server.listening}`)
 })
--- packages/registry/src/server.ts
+++ packages/registry/src/server.ts
@@ -1,12 +1,15 @@
+import * as http from "http";
 import app from './app'
 
 import { RegistryOptions } from 'registry-options';
  
-const startRegistry = (options: RegistryOptions) => {
-  const port = options.port || 3000
-  app.listen(port, () => {
-    console.log(`Loom Registry server started on: ${port}`);
-  });
-}
-
+const startRegistry  = (options: RegistryOptions) : Promise<http.Server> => {
+  return new Promise(resolve => {
+    const port = options.port || 3000
+    const server = app.listen(port, () => {
+      resolve(server)
+    });
+  })
+ }
+  
 export default startRegistry
